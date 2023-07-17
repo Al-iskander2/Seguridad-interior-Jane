5e4673ecf565feaf438b4469c2d0739390960d12
alejandro-rangel-62457815b <113322588+Al-iskander2@users.noreply.github.com>
2023-07-16 19:40:40 -0600

    Update index.js
    we

--- index.js
+++ index.js
@@ -1,55 +1,5 @@
-const alfabeto = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-const displayText = text => {
-    inputOriginal.value = text;
-}
+const inputOriginal = document.getElementById('input-original');
 
 function displayText(text) {
-    inputOriginal.textContent = text;
+    inputOriginal.value = text;
 }
-
-
-<!--
-Day1:
-Yo te apoyo.
-
-Day2:
-Eres hermosa. 
-
-Day3:
-Eres bonita.
-
-Day4:
-Creo en ti.
-
-Day5:
-Esta bien sentirte triste en ocasiones.
-
-Day6:
-¿En qué te puedo ayudar? 
-
-Day7:
-No estás sola, yo estoy contigo.
-
-Day8:
-Tranquila, todo pasará, ya verás.
-
-Day9:
-Me encantas. 
-
-Day10:
-Me vuelvo loc@ por ti.
-
-Day11:
-Me inspiras.
-
-Day12:
-Eres alguien especial, diferente y eso me gusta.
-
-Day13:
-Gracias por estar.
-
-Day14:
-Eres increíble, no lo dudes.
-
-Day15:
-Jamás dejes de creer en ti, que yo no lo hago. -->
